b2408e4768f9f602aacd2e9d9dff1021bbeba513
Mahmoud Laban <Mahmoud.Laban@gmail.com>
2022-05-23 00:26:34 +0100

    InSight project

--- HMIUserApp/MainWindow.xaml.cs
+++ HMIUserApp/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using EasyModbus;
 using HMIUserApp.Model;
 using HMIUserApp.Service;
+using Microsoft.Win32;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -33,12 +36,15 @@ namespace HMIUserApp
         private DispatcherTimer aTimer;
         private DispatcherTimer csvTimer;
         private int timerInterval = 1;
-        private readonly int csvTimerInterval = 10; // minutes
+        private readonly int csvTimerInterval = 20; // minutes
         private readonly int quantity = 50;
         private bool isLive = true;
         private bool isSaveDatabase = true;
         private bool isSaveCsv = true;
+        private bool isUploadCsv = true;
         private readonly string folderName = "HistoryData";
+        private readonly string archiveFolderName = "ArchiveData";
+        private readonly string logFileName = "logs.csv";
         private string csvFileName;
         private readonly LogDataService _logDataService = new LogDataService();
         private DateTime startDate;
@@ -46,13 +52,13 @@ namespace HMIUserApp
         public MainWindow()
         {
             InitializeComponent();
-            
+
             liveData = new List<LogData>();
             DGLogs.ItemsSource = liveData;
-                
+
             timerInterval = Convert.ToInt32(TxtInterval.Text);
             aTimer = new DispatcherTimer();
-            aTimer.Interval = TimeSpan.FromMilliseconds(Convert.ToInt32(timerInterval* 1000));
+            aTimer.Interval = TimeSpan.FromMilliseconds(Convert.ToInt32(timerInterval * 1000));
             aTimer.Tick += OnTimedEvent;
 
             csvTimer = new DispatcherTimer();
@@ -61,7 +67,13 @@ namespace HMIUserApp
 
             FetchMinMaxDate();
 
-
+            if (!File.Exists(logFileName))
+            {
+                using (StreamWriter sw = File.AppendText(logFileName))
+                {
+                    sw.WriteLine($"FileName, Status, DateTime");
+                }
+            }
         }
         private void OnConnect(object sender, RoutedEventArgs e)
         {
@@ -159,11 +171,15 @@ namespace HMIUserApp
             } else if (toggleSwitch.Tag?.ToString() == "SaveDatabase")
             {
                 isSaveDatabase = true;
-            } else
+            } else if (toggleSwitch.Tag?.ToString() == "SaveCsv")
             {
                 isSaveCsv = true;
             }
-            
+            else if (toggleSwitch.Tag?.ToString() == "UploadCSVDATA")
+            {
+                isUploadCsv = true;
+            }
+
         }
         private void OnUnChecked(object sender, RoutedEventArgs e)
         {
@@ -180,9 +196,15 @@ namespace HMIUserApp
             {
                 isSaveDatabase = false;
             }
-            else
+            else if (toggleSwitch.Tag?.ToString() == "SaveCsv")
             {
                 isSaveCsv = false;
+                tsUpload.IsChecked = false;
+                isUploadCsv = false;
+            }
+            else if (toggleSwitch.Tag?.ToString() == "UploadCSVDATA")
+            {
+                isUploadCsv = false;
             }
         }
         private void TagsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -281,8 +303,25 @@ namespace HMIUserApp
 
         private void OnCsvFileNameEvent(Object source, EventArgs e)
         {
+            if(isUploadCsv)
+            {
+                string pathString = System.IO.Path.Combine(folderName, csvFileName);
+                UploadCSV(pathString);
+            }
+            
             csvFileName = $"ModbusData_{DateTime.Now.ToString("yyyy'_'MM'_'dd'_'HH'_'mm")}.csv";
         }
+
+        private void OnUploadCsv(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV Files (*.csv)|*.csv";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                UploadCSV(openFileDialog.FileName); 
+            }
+        }
+
         private void AddLogData(int address, int value, string tag, int i)
         {
             if (modbusTag == tag)
@@ -314,15 +353,19 @@ namespace HMIUserApp
                 {
                     using (StreamWriter sw = File.AppendText(pathString))
                     {
-                        sw.WriteLine($"{tag + String.Format("{0:D5}", i + 1)}, {DateTime.Now.ToString()}, {value.ToString()}, {String.Format("{0:D5}", address)}");
+                        //sw.WriteLine($"{tag + String.Format("{0:D5}", i + 1)}, {DateTime.Now.ToString()}, {value.ToString()}, {String.Format("{0:D5}", address)}");
+                        sw.WriteLine($"{tag + String.Format("{0:D5}", i + 1)}, {DateTime.UtcNow.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")}, {value.ToString()}");
                     }
                 }
                 else
                 {
                     using (StreamWriter sw = File.AppendText(pathString))
                     {
-                        sw.WriteLine($"TagName, DateTime, Value, ModbusAddrss");
-                        sw.WriteLine($"{tag + String.Format("{0:D5}", i + 1)}, {DateTime.Now.ToString()}, {value.ToString()}, {String.Format("{0:D5}", address)}");
+                        //sw.WriteLine($"TagName, DateTime, Value, ModbusAddress");
+                        //sw.WriteLine($"{tag + String.Format("{0:D5}", i + 1)}, {DateTime.Now.ToString()}, {value.ToString()}, {String.Format("{0:D5}", address)}");
+
+                        sw.WriteLine($"TagName, DateTime, Value");
+                        sw.WriteLine($"{tag + String.Format("{0:D5}", i + 1)}, {DateTime.UtcNow.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'")}, {value.ToString()}");
                     }
                 }
                 
@@ -396,6 +439,50 @@ namespace HMIUserApp
                 aTimer.Stop();
                 aTimer.Interval = TimeSpan.FromMilliseconds(Convert.ToInt32(timerInterval * 1000));
                 aTimer.Start();
+            }
+            
+        }
+
+
+        private void UploadCSV(string pathString)
+        {
+            if (File.Exists(pathString))
+            {
+                string content;
+                using (var reader = new StreamReader(pathString))
+                {
+                    content = reader.ReadToEnd();
+                }
+                var client = new RestClient("https://online.wonderware.com/apis");
+                var request = new RestRequest("upload/datasource", Method.Post);
+                request.AddHeader("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjRkNGQyYzY4LTFkNjEtNDQwZi05ZWQyLTQwYzdmYThlMWFmMyJ9.eyJEYXRhU291cmNlSWQiOiJiMGIyYWU2Zi05MzU4LTQ2NzAtOTZkZC0xYjc0YWQ0N2EyYjAiLCJ0eXBlIjoic2VydmljZSIsInZlcnNpb24iOiIyLjAiLCJ0ZW5hbnRpZCI6ImY0NWE1YmYxLTFmMDUtNDI2Ny04MDRiLWZkNWFkMmEwNzg0NCIsInNpaWQiOiIwNDgxNTUwYy0xOThiLTQyZTktYTk3Mi1hYmViOWZhZDliNTIiLCJqdGkiOiJkNjkxMjBmZC1lMTYyLTRmM2EtODU5ZS1iMDMzNTNjMTdhNjUiLCJpc3MiOiJwcm9vZm9maWRlbnRpdHlzZXJ2aWNlIn0.M4G-8ihM5g_bBdJFZIOIJkl9hBLwYRQzVgPLU6AND50tcoqVX6WCAxXjKOBwfYSC7DuvI6nMx3Ttt7fUiCyMqaVwlPIVpHQzKrkqAVOWZU8IIGfC2R1tsEK1HJXbbvnyZTmRapLk3ULzwakAkxTVh0PyTAhmrdw4pdlDWzn0xpk_TyEsOj3k8MAnBudiGEdO1-fL8VnpdMzIdpcTOe90n9Rd1k7h-HvH9DcvswWXd7bUvc5ttmkmYNdvlQhnG3Gz66gxgngNu9b3Gj2v98SEVFODe-tKLgU_il9aXD2BY-F_iyl-kj0TVE53MAOMHJ43bFi3wLWHu2elvIF6EPBkjQ");
+                request.AddHeader("x-filename", System.IO.Path.GetFileName(pathString));
+                request.AddHeader("Content-Type", "text/plain");
+
+                request.AddParameter("text/plain", content, ParameterType.RequestBody);
+
+                var response = client.ExecuteAsync(request).Result;
+                string fileName = System.IO.Path.GetFileName(pathString);
+                if (response.StatusCode == HttpStatusCode.Accepted)
+                {
+                    LblLog.Text = "Upload csv file successfully";
+                    string archiveFilePath = System.IO.Path.Combine(archiveFolderName, fileName);
+                    Directory.CreateDirectory(archiveFolderName);
+                    File.Move(pathString, archiveFilePath);
+                    using (StreamWriter sw = File.AppendText(logFileName))
+                    {
+                        sw.WriteLine($"{fileName}, {"UPLOADED SUCCESS"}, {DateTime.Now.ToString()}");
+                    }
+                }
+                else
+                {
+                    LblLog.Text = response.Content != null ? response.Content.ToString() : "Something error";
+                    using (StreamWriter sw = File.AppendText(logFileName))
+                    {
+                        sw.WriteLine($"{fileName}, {"UPLOADED FAILED"}, {DateTime.Now.ToString()}");
+                    }
+                }
+                
             }
             
         }
